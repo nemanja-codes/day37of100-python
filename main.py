fc41993e70d03f4ec00a13328c3b2705a4bbb47b
Nemanja Markovic <necamark@gmail.com>
2024-06-12 21:23:00 +0200

    "creating graph on Pixela; using HTTP Header"

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import requests
 
+TOKEN = "jifbokN293Okskl"
+USERNAME = "djavo"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "jifbokN293Okskl",
-    "username": "djavo",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph001",
+    "name": "Walking Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
